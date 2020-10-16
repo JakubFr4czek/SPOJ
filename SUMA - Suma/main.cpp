c581188d9188760208010c423a08338b637cbbfb
JakubFr4czek <72702709+JakubFr4czek@users.noreply.github.com>
2020-10-16 23:46:14 +0200

    Create main.cpp

---
+++ SUMA - Suma/main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <conio.h>
+
+using namespace std;
+
+int main()
+{
+    int a,b=0;
+
+    while(cin>>a)
+    {
+        b+=a;
+        cout<<b<<endl;
+
+
+    }
+    return 0;
+}
